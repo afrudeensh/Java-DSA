e3a0e3d8a0e74d1db5efa44a9c0465cfe60c523a
Afrudeen S H <115522869+afrudeensh@users.noreply.github.com>
2025-05-28 20:34:09 +0530

    Update RemovingEvenIntegers.java

--- src/Arrays/RemovingEvenIntegers.java
+++ src/Arrays/RemovingEvenIntegers.java
@@ -3,9 +3,14 @@ package Arrays;
 public class RemovingEvenIntegers {
     public static void main(String[] args) {
         int[] num = {1, 20, 30, 40, 5, 60};
+        System.out.print("Before removing even elements ");
+        for (int a = 0; a < num.length; a++) {
+            System.out.print(num[a]+" ");
+        }
+        System.out.print("\nAfter removing even elements ");
         for (int i = 0; i < num.length; i++) {
             if (num[i] % 2 != 0) {
-                System.out.println(num[i]);
+                System.out.print(num[i]+" ");
             }
         }
 
